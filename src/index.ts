c43ba8b684df45187e2fc77a41a06eb36fb58667
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-08-23 21:45:04 +0545

    add types to parameter

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-//@ts-ignore
-function addNumbers(a, b) {
+function addNumbers(a: number, b: number) {
   return a + b;
 }
 console.log(addNumbers(2, 10));
 console.log(addNumbers(2, 3));
-console.log(addNumbers(2, '10'));
+console.log(addNumbers(2, 10));
