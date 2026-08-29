3a7d193f8a98eb32ba89dedea145bc850dedcaf4
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-08-29 10:42:21 +0545

    unknown type

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-let firstName: string = 'Mark';
-firstName = 123;
-firstName = [];
-
-function returnParams(param) {
-  return param;
+function multiplyByTwo(number: unknown) {
+  if (typeof number === 'number') {
+    return number * 2;
+  }
+  throw new Error('Input must be a number');
 }
+const result = multiplyByTwo(5);
+// const result2 = multiplyByTwo('5'); // This will throw an error
+console.log(result);
+// console.log(result2);
