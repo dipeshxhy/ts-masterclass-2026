9fe638c5b638c91569c3eb7e2bfed4e40e5ad15b
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-08-29 09:19:59 +0545

    number string boolean type

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,30 @@
-function addNumbers(a: number, b: number) {
-  return a + b;
-}
-console.log(addNumbers(2, 10));
-console.log(addNumbers(2, 3));
-console.log(addNumbers(2, 10));
+// 1. number 2.string 3.boolean 4.null 5.undefined 6.object 7.symbol 8.bigint
+/**
+ * string types
+ */
+let firstName = 'John';
+let lastName = 'Doe';
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+// inference :guessing types
+let automobile = 'BMW';
+console.log(automobile);
+
+const city = 'New York';
+console.log(city);
+
+let students = 32;
+let studentsAsString = students.toString();
+console.log(studentsAsString);
+
+// number types
+let age = 32;
+let year = 2026;
+const numberOfMembers = 45;
+// numberOfMembers = 100;
+let stringToNumber = parseInt('1985');
+
+// boolean
+let isStudent = true;
+const asAlwaysStudent = true;
