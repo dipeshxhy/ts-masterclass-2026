c31c1ccb8322d7ec46557ae9c474b65c83651aed
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-08-29 09:36:18 +0545

    null and undefined

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,12 @@
-// 1. number 2.string 3.boolean 4.null 5.undefined 6.object 7.symbol 8.bigint
-/**
- * string types
- */
-let firstName = 'John';
-let lastName = 'Doe';
-let fullName = firstName + ' ' + lastName;
-console.log(fullName);
+let user;
+console.log(user);
 
-// inference :guessing types
-let automobile = 'BMW';
-console.log(automobile);
+// null
+let userRole = null;
+console.log(userRole);
 
-const city = 'New York';
-console.log(city);
+console.log(undefined === null);
 
-let students = 32;
-let studentsAsString = students.toString();
-console.log(studentsAsString);
-
-// number types
-let age = 32;
-let year = 2026;
-const numberOfMembers = 45;
-// numberOfMembers = 100;
-let stringToNumber = parseInt('1985');
-
-// boolean
-let isStudent = true;
-const asAlwaysStudent = true;
+if (!userRole) {
+  console.log('User role is null');
+}
