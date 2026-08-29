6054693d802beab00b89ef7dd8d889de392fc0e1
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-08-29 09:47:53 +0545

    bigint type

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
-let user;
-console.log(user);
+// bigint
 
-// null
-let userRole = null;
-console.log(userRole);
+const safeInt = Number.MAX_SAFE_INTEGER;
 
-console.log(undefined === null);
+console.log(safeInt);
 
-if (!userRole) {
-  console.log('User role is null');
-}
+const safeIntPlusOne = safeInt + 1;
+const safeIntPlusTwo = safeInt + 2;
+
+console.log(safeIntPlusOne);
+console.log(safeIntPlusTwo);
+
+const bigIntPlusOne = BigInt(safeInt) + BigInt(1);
+const bigIntPlusTwo = BigInt(safeInt) + BigInt(2);
+
+console.log(bigIntPlusOne);
+console.log(bigIntPlusTwo);
+
+let f = Math.log(10);
