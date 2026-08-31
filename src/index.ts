1a43adbe49ab631d56b7a80f6aa09bc6b2d00c0f
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-08-31 08:49:10 +0545

    nested object type

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
+type Author = {
+  name: string;
+  email: string;
+  age: number;
+};
 type Post = {
   title: string;
   content: string;
   date: Date;
   category?: string;
+  author: Author;
 };
 let post: Post = {
   title: 'This is a blog post',
   content: 'content of the post',
   date: new Date(),
-};
-let post2: Post = {
-  title: 'This is a blog post',
-  content: 'content of the post',
-  date: new Date(),
-  category: 'AI',
+  author: {
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    age: 30,
+  },
 };
