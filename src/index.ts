daad118f178733458d799a4164ef152fcb5f0536
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-08-31 08:14:39 +0545

    type hierarchy

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,12 @@
-// union types
-// combine two types together its union type
-type StringOrNumber = string | number;
-type NumberOrUndefined = number | undefined;
-type StringOrNumberOrUndefined = StringOrNumber | undefined;
-type DateOrUndefined = Date | undefined;
+type check = any extends unknown ? true : false;
+type check2 = string extends any ? true : false;
+type check3 = undefined extends any ? true : false;
+type check4 = [] extends object ? true : false;
+type check5 = (() => void) extends object ? true : false;
 
-let stringOrNumber: StringOrNumber = 123;
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
 
-function print(input?: string) {
-  if (input === undefined) {
-    console.log('No input provided');
-  } else {
-    console.log(input);
-  }
-}
-print('Hello, World!');
-print();
+let strings = ['a', 'b', 'c'];
+let myFunc = () => 2;
