f145854f44f4d065ca989e86154d9cee3951d07c
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-09-01 23:14:37 +0545

    solution to practice

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,33 @@
-type Cat = {
+type Caterer = {
   name: string;
-  purrs: boolean;
-  color: string;
+  address: string;
+  phone: number;
 };
-type Dog = {
-  name: string;
-  barks: boolean;
-  color: string;
+type SeatDetails = {
+  [seatNumber: string]: string; // seat number as key and passenger name as value
 };
-type HybridAnimal = Cat & Dog;
+type Airplane = {
+  model: string;
+  flightNumber: string;
+  timeOfDeparture: Date;
+  timeOfArrival: Date;
+  caterer: Caterer;
+  seats: SeatDetails;
+};
 
-let hybridAnimal: HybridAnimal = {
-  name: 'Fluffy',
-  color: 'brown',
-  purrs: true,
-  barks: true,
+let airplane: Airplane = {
+  model: 'Airbus A380',
+  flightNumber: 'A2201',
+  timeOfDeparture: new Date(),
+  timeOfArrival: new Date(),
+  caterer: {
+    name: 'Special Food Ltd',
+    address: '484, Some Street, New York',
+    phone: 7867856751,
+  },
+  seats: {
+    A1: 'John Doe',
+    A2: 'Mark Doe',
+    A3: 'Sam Doe',
+  },
 };
