fa39788eb91a98603662fc7e886cfa588e39de11
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-09-02 00:31:43 +0545

    enums

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,52 @@
-let number: readonly number[] = [1, 2, 3];
+const STATUS_LOADING = 'loading';
+const STATUS_SUCCESS = 'success';
+const STATUS_ERROR = 'error';
 
-// number.push(4) // Error: Property 'push' does not exist on type 'readonly number[]'.
-type ReadonlyTuple = readonly [number, string, boolean];
-const readonlyTuple: ReadonlyTuple = [42, 'hello', true];
+// enums
+enum Direction {
+  Up,
+  Down,
+  Left,
+  Right,
+}
+Direction.Up;
+console.log(Direction.Right);
 
-// alternative syntax
-type ReadonlyTupleAlt = Readonly<[number, string, boolean]>;
-const readonlyTupleAlt: ReadonlyTupleAlt = [42, 'hello', true];
+enum Direction2 {
+  Up = 1,
+  Down,
+  Left,
+  Right,
+}
+Direction2.Up;
+console.log(Direction2.Right);
+
+enum Roles {
+  Admin = 'admin',
+  User = 'user',
+  Guest = 'guest',
+}
+Roles.Admin;
+console.log(Roles.Guest);
+
+type Person = {
+  name: string;
+  email: string;
+  password: string;
+  role: Roles;
+};
+
+const person: Person = {
+  name: 'John Doe',
+  email: 'john.doe@example.com',
+  password: 'password123',
+  role: Roles.Admin,
+};
+
+console.log(person);
+
+enum Direction3 {
+  Up = 1,
+  Down = 'down',
+  Left = 3,
+}
