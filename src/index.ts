435050dd75c11c113560d16740d43cc3d4291e9a
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-09-01 21:02:56 +0545

    union type

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,35 @@
-type Author = {
+type Dog = {
   name: string;
-  email: string;
-  age: number;
-  readonly type: 'human' | 'ai';
-};
-type AwardDetails = {
-  name: string;
-  date: Date;
-};
-type Awards = {
-  [key: string]: AwardDetails;
-};
-type Post = {
-  title: string;
-  content: string;
-  date: Date;
-  category?: string;
-  author: Author;
-  awards: Awards;
-};
-let post: Post = {
-  title: 'This is a blog post',
-  content: 'content of the post',
-  date: new Date(),
-  author: {
-    name: 'John Doe',
-    email: 'john.doe@example.com',
-    age: 30,
-    type: 'human',
-  },
-  awards: {
-    web: {
-      name: 'web award',
-      date: new Date(),
-    },
-    web3: {
-      name: 'web3 award',
-      date: new Date(),
-    },
-  },
-  category: 'Technology',
+  barks: boolean;
+  wags: boolean;
 };
 
-let post2: Post = {
-  title: 'This is a blog post',
-  content: 'content of the post',
-  date: new Date(),
-  author: {
-    name: 'John Doe',
-    email: 'john@mail.com',
-    age: 30,
-    type: 'ai',
-  },
-  awards: {},
+type Cat = {
+  name: string;
+  purrs: boolean;
+};
+
+type DogAndCatUnion = Dog | Cat;
+
+let dog: DogAndCatUnion = {
+  name: 'Buddy',
+  barks: true,
+  wags: true,
+};
+let cat: DogAndCatUnion = {
+  name: 'Whiskers',
+  purrs: true,
+};
+
+let hybridAnimal: DogAndCatUnion = {
+  name: 'Fido',
+  barks: true,
+  purrs: true, // This will cause a TypeScript error because 'purrs' is not a property of Dog
+};
+
+let partialDog: DogAndCatUnion = {
+  name: 'Rex',
+  purrs: true, // This will cause a TypeScript error because 'purrs' is not a property of Dog
+  barks: true,
+  wags: true,
 };
