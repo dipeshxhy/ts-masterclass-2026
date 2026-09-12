92241ea2d7222247fe4af161c9a3c88dc4ce67be
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-09-12 10:29:39 +0545

    practice enums

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,30 @@
-enum ShapeKind {
-  Circle = 'circle',
-  Square = 'square',
+// Practice Questions
+//* 1. Create an array numbers that only accepts numbers and another array strings that only accepts strings.
+
+let numbers: number[] = [1, 2, 3, 4, 5];
+let strings: string[] = ['apple', 'banana', 'cherry'];
+
+//* 2. Create a tuple person that holds a string (name) and a number (age).
+
+let person: [string, number] = ['Alice', 30];
+
+//* 3. Create a readonly array colors that holds strings and a readonly tuple point that holds two numbers (x, y). Attempt to modify their elements and observe the TypeScript error.
+
+let colors: readonly string[] = ['red', 'green', 'blue'];
+let point: readonly [number, number] = [10, 20];
+colors[0] = 'yellow'; // Error: Index signature in type 'readonly string[]' only permits reading
+point[0] = 30; // Error: Index signature in type 'readonly [number, number]' only permits reading
+
+//* 4. Create an enum called StatusEnum that should 3 properties Active, Inactive, Pending
+enum StatusEnum {
+  Active,
+  Inactive,
+  Pending,
 }
 
-type Circle = {
-  kind: ShapeKind.Circle;
-  radius: number;
-};
-
-type Square = {
-  kind: ShapeKind.Square;
-  sideLength: number;
-};
-
-let circle: Circle = {
-  radius: 100,
-  kind: ShapeKind.Circle,
-};
-
-function printShape(shape: ShapeKind) {
-  console.log(shape);
-}
-// printShape(ShapeKind)
-printShape(ShapeKind.Circle);
-printShape(ShapeKind.Square);
+//* 5. Create an object as const called Status with the same structure as an StatusEnum
+const Status = {
+  Active: StatusEnum.Active,
+  Inactive: StatusEnum.Inactive,
+  Pending: StatusEnum.Pending,
+} as const;
