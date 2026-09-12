3e52a8efa9d34f80c9307009dc069ffd81abcafd
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-09-12 17:28:49 +0545

    async function

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
-function writeToDatabase(value: string) {
-  console.log('writing to database', value);
-}
-function throwError(message: string): never {
-  throw new Error(message);
-}
+async function fetchFromDatabase(id: number) {}
+const anotherAsyncFunction = async () => {};
 
-type check = never extends any ? true : false;
-type check2 = void extends never ? true : false;
+async function returnString(id: number): Promise<string> {
+  return Promise.resolve('Hello World');
+}
+type User = {
+  name: string;
+  age: number;
+};
+
+async function returnUser(id: number): Promise<User> {
+  return Promise.resolve({ name: 'John Doe', age: 30 } as User);
+}
