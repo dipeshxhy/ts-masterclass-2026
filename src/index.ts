7356f3f621bfe50a1bfbc1e9bdb40f48d11af2cb
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-09-12 17:16:51 +0545

    anonymous function

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,4 @@
-type GreetingFun = (greeting: string, additionalInfo?: string) => string;
-type Person = {
-  name: string;
-  age: number;
-  ageUnit: 'years' | 'months' | 'days';
-  // greet:function;
-  greet: GreetingFun;
-};
-const person: Person = {
-  name: 'Scout',
-  age: 30,
-  ageUnit: 'years',
-  greet: (greeting: string, additionalInfo?: string) => {
-    if (additionalInfo) {
-      return `${greeting}, ${additionalInfo}`;
-    }
-    return `${greeting}, ${person.name}`;
-  },
-};
+const students = ['Alice', 'Bob', 'Charlie', 'David', 'Eve'];
 
-function convertAgeToMonths(person: Person): Person {
-  if (person.ageUnit === 'years') {
-    return {
-      ...person,
-      age: person.age * 12,
-      ageUnit: 'months',
-    };
-  }
-  return person;
-}
-console.log(convertAgeToMonths(person));
-
-console.log(person.greet('hello'));
+const studentNames = students.map((std) => console.log(std));
+// console.log(studentNames);
